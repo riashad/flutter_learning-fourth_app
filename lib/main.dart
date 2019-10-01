2e0848198b963eae80026c476476b97b22466fb6
Riashad Noor <riashad.noor@gmail.com>
2019-10-02 02:24:08 +0600

    My Personal Business Card v0.2

--- lib/main.dart
+++ lib/main.dart
@@ -48,74 +48,80 @@ class PersonalBusinessCard extends StatelessWidget {
                 height: 20.0,
 
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
-                padding: EdgeInsets.all(15.0),
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Icon(
-                      Icons.call,
-                      color: Colors.teal.shade900,
-                    ),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Icon(
+                        Icons.call,
+                        color: Colors.teal.shade900,
+                      ),
 
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '+880-1715-002091',
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.teal.shade900,
-                        letterSpacing: 2.5,
+                      SizedBox(
+                        width: 10.0,
                       ),
-                    ),
-                  ],
+                      Text(
+                        '+880-1715-002091',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade900,
+                          letterSpacing: 2.5,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
-                padding: EdgeInsets.all(15.0),
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.email),
-                      color: Colors.teal.shade900,
-                      onPressed: (){
-                        print('Email Button Pressed');
-                      },
-                    ),
-                    SizedBox(
-                      width: 0.0,
-                    ),
-                    Text(
-                      'hello@riashad.net',
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.teal.shade900,
-                        letterSpacing: 2.5,
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 10.0,
                       ),
-                    ),
-                  ],
+                      IconButton(
+                        icon: Icon(Icons.email),
+                        color: Colors.teal.shade900,
+                        onPressed: (){
+                          print('Email Button Pressed');
+                        },
+                      ),
+                      SizedBox(
+                        width: 0.0,
+                      ),
+                      Text(
+                        'hello@riashad.net',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade900,
+                          letterSpacing: 2.5,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
-                padding: EdgeInsets.only(left: 30.0),
-                child: Row(
-                  children: <Widget>[
-                    Image.asset('assets/images/Riashad_QR_Code.png'),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.only(left:50.0),
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset('assets/images/Riashad_QR_Code.png'),
+                    ],
+                  ),
                 ),
               ),
             ],
